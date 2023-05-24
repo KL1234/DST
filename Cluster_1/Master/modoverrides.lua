e2c15d95cba14a787a1feb7134df6fdc56647647
KL <d23584d321@gmail.com>
2023-05-24 21:57:50 +0800

    “第一次上傳世界

---
+++ Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,15 @@
+return {
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  } 
+}
