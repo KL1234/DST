b8b3af912d4bfa8d57ba6898f75e24dba3dac4c6
KL <d23584d321@gmail.com>
2023-05-25 21:52:25 +0800

    KL12345 第一天黃昏

--- Cluster_3/Caves/modoverrides.lua
+++ Cluster_3/Caves/modoverrides.lua
@@ -1,17 +1,4 @@
 return {
-  ["workshop-1378549454"]={
-    configuration_options={
-      ["MemSpikeFix:"]=false,
-      MemSpikeFixmaster_override=true,
-      ["MemSpikeFixworkshop-1901927445"]="default",
-      ["MemSpikeFixworkshop-345692228"]="default",
-      ["MemSpikeFixworkshop-362175979"]="default",
-      ["MemSpikeFixworkshop-365119238"]="default",
-      ["MemSpikeFixworkshop-378160973"]="default",
-      craftinghighlight=false 
-    },
-    enabled=true 
-  },
   ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
   ["workshop-378160973"]={
     configuration_options={
